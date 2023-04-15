6e9438c66a2fd4e2657e7ae602ee5d4aa5a125be
JackitudilinksG <73828512+JackitudilinksG@users.noreply.github.com>
2023-04-15 14:36:33 +0400

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,82 @@
 import 'package:flutter/material.dart';
-import 'profile.dart';
+ import 'profile.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: Home(),
-  ),
-  );
-}
+ void main() {
+   runApp(const MaterialApp(
+     home: Home(),
+   ),
+   );
+ }
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  class Home extends StatelessWidget {
+    const Home({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
 
-      appBar: AppBar(
+        appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              'Hamico',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.person),
+              tooltip: "Save Todo and Retrun to List",
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Profile()),
+                );
+              },
+            ),
+          ],
+
+          backgroundColor: Colors.purple[500],
+      ),
+
+
+        body: Center(
+          child: Column(
+            children: const [
+              SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Image.asset('assets/arrow.png'),
+        ),
+
+        backgroundColor: Colors.lightBlue[50],
+      );
+    }
+  }
+
+
+
+
+  class WaterIntake extends StatelessWidget {
+    const WaterIntake({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+
+        appBar: AppBar(
           centerTitle: true,
           title: const Text(
-            'Hamico',
+            'Water Intake',
             style: TextStyle(
               fontSize: 25.0,
               fontWeight: FontWeight.bold,
@@ -26,135 +84,77 @@ class Home extends StatelessWidget {
               fontFamily: 'Pacifico',
             ),
           ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.person),
-            tooltip: "Save Todo and Retrun to List",
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Profile()),
-              );
-            },
-          ),
-        ],
-
-        backgroundColor: Colors.purple[500],
-    ),
-
-
-      body: Center(
-        child: Column(
-          children: const [
-            SizedBox(
-              height: 50,
-            ),
-          ],
+          backgroundColor: Colors.purple[500],
         ),
-      ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Image.asset('assets/arrow.png'),
-      ),
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.home,
+                ),
+                title: const Text('Page 1'),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Home()));
+                },
+              ),
 
-      backgroundColor: Colors.lightBlue[50],
-    );
-  }
-}
+              ListTile(
+                leading: const Icon(
+                  Icons.home,
+                ),
+                title: const Text('Page 2'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
 
+              ListTile(
+                leading: const Icon(
+                  Icons.access_time_filled,
+                ),
+                title: const Text('Page 3'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
 
-
-
-class WaterIntake extends StatelessWidget {
-  const WaterIntake({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Water Intake',
-          style: TextStyle(
-            fontSize: 25.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-            fontFamily: 'Pacifico',
+            ],
           ),
         ),
-        backgroundColor: Colors.purple[500],
-      ),
-
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.purple,
+        body: Center(
+          child: Column(
+            children: const [
+              SizedBox(
+                height: 50,
               ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.home,
+              Text (
+                  'hello',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
               ),
-              title: const Text('Page 1'),
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Home()));
-              },
-            ),
-
-            ListTile(
-              leading: const Icon(
-                Icons.home,
-              ),
-              title: const Text('Page 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-
-            ListTile(
-              leading: const Icon(
-                Icons.access_time_filled,
-              ),
-              title: const Text('Page 3'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-
-          ],
+            ],
+          ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          children: const [
-            SizedBox(
-              height: 50,
-            ),
-            Text (
-                'hello',
-                textAlign: TextAlign.center,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
-            ),
-          ],
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Image.asset('assets/arrow.png'),
         ),
-      ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Image.asset('assets/arrow.png'),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-
-      backgroundColor: Colors.lightBlue[50],
-    );
+        backgroundColor: Colors.lightBlue[50],
+      );
+    }
   }
-}
 
