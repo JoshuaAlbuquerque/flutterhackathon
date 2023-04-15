1c67f57ca18efd7272b25c34e40a8c4f79520435
JoshuaAlbuquerque <22f43.joshua@sjec.ac.in>
2023-04-15 16:24:14 +0530

    proper push

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_grid_button/flutter_grid_button.dart';
 import 'package:css_colors/css_colors.dart';
 
-import 'WaterIntake.dart';
 import 'profile.dart';
 
 void main() {
@@ -61,110 +60,6 @@ class Home extends StatelessWidget {
         onPressed: null,
         child: Image.asset('assets/arrow.png'),
       ),
-
-      backgroundColor: Colors.lightBlue[50],
-    );
-  }
-}
-
-
-
-
-class WaterIntake extends StatelessWidget {
-  const WaterIntake({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Water Intake',
-          style: TextStyle(
-            fontSize: 25.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-            fontFamily: 'Pacifico',
-          ),
-        ),
-        backgroundColor: Colors.purple[500],
-      ),
-
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.purple,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.home,
-              ),
-              title: const Text('Page 1'),
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Home()));
-              },
-            ),
-
-            ListTile(
-              leading: const Icon(
-                Icons.home,
-              ),
-              title: const Text('Page 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-
-            ListTile(
-              leading: const Icon(
-                Icons.access_time_filled,
-              ),
-              title: const Text('Page 3'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-
-          ],
-        ),
-      ),
-      body:GridView(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-      body: Center(
-        child: Column(
-          children: const [
-            SizedBox(
-              height: 50,
-            ),
-            Text (
-                'hello',
-                textAlign: TextAlign.center,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
-            ),
-          ],
-        ),
-        children: [
-          Image.network('https://picsum.photos/250?image=1'),
-          Image.network('https://picsum.photos/250?image=2'),
-
-        ],
-      ),
-
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Image.asset('assets/arrow.png'),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
 
       backgroundColor: Colors.lightBlue[50],
     );
